30ec1ae4f3618639b68eef440234d0611ec04050
sendy <ultramilk500@gmail.com>
2026-01-07 21:15:45 +0700

    'transaksi

---
+++ app/Http/Controllers/TransaksiController.php
@@ -0,0 +1,7 @@
+class KasirController extends Controller
+{
+    public function index() {}
+    public function create() {}
+    public function store(Request $request) {} // ⬅ PAYMENT DI SINI
+    public function transaksi() {}
+}
